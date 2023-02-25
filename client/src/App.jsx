79946b264260b633fd1d385f6e0693a4830d258c
IgnacioPerezEtc <nachoperezetc@gmail.com>
2023-02-25 15:44:58 -0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Technologies from "./components/Technologies/Technologies";
 import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <BrowserRouter>
