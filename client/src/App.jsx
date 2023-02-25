9b6db31987bd3658ce448596200a570ab41e737f
Perez Etchegaray Manuel <manuel.perezetchegaray@bonzzu.com>
2023-02-25 15:45:40 -0300

    agregando modo negro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,26 @@ import Home from "./components/Home";
 import Technologies from "./components/Technologies/Technologies";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function handleDarkModeToggle() {
+    setDarkMode(!darkMode);
+  }
+
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className={darkMode ? "App dark-mode" : "App"}>
+        {/* Agrega un botón que permita cambiar entre el modo claro y oscuro */}
+        <button onClick={handleDarkModeToggle}>
+          Cambiar a {darkMode ? "modo claro" : "modo oscuro"}
+        </button>
         <Routes>
-        <Route exact path={"/"} element={<Home/>} />
-        <Route exact path={"/tecnologias"} element={<Technologies/>} />
-        <Route exact path={"/proyectos"} element={<Projects/>} />
-        <Route exact path={"/contactame"} element={<Contact/>} />
+          <Route exact path={"/"} element={<Home />} />
+          <Route exact path={"/tecnologias"} element={<Technologies />} />
+          <Route exact path={"/proyectos"} element={<Projects />} />
+          <Route exact path={"/contactame"} element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
